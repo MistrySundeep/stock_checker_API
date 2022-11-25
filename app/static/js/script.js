0f6b5ec0469932a7069e0195ebe06e56af5065a9
Sundeep <mistrysundeep@gmail.com>
2022-11-25 21:53:08 +0000

    API GET call working using JQuery
    TODO:
    
    On document load, run get call for all items and create cards for each item showing each item's information

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,13 @@
-function demo() {
-    console.log('Hello World');
-}
-
-demo();
+$("#sub_btn").click(function (){
+    $.get('/show_all_items', function(data, status){
+        console.log(`${status}`)
+        console.log(`${data.size}`)
+        for(let i = 0; i<data.length; i++){
+            console.log("ITEM " + i)
+            $.each(data[i], function(key, val){
+                console.log("Key: " + key + ", Value: " + val);
+                $(".appended_data").append("<p>" + key + ": " + val + "</p>");
+            });
+        }
+    });
+});
