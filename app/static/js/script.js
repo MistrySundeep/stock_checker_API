aa14cda5dae3bf1f861412bf70c45b61d2f5bea1
Sundeep <mistrysundeep@gmail.com>
2022-12-15 23:26:43 +0000

    Changed page layout to table instead of grid of cards.
    Working on deleting items next

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,32 +1,25 @@
 $(document).ready(function (){
     $.get('/show_all_items', function(data, status){
-        let $elem = $('body div#append_data');
-
-
         for(let i = 0; i < data.length; i++){
-            console.log("ITEM " + i)
-
-            $elem.append(
-//            Move this incline css out to a stylesheet later
-                $('<div/>', {'id': 'item' + i, 'class': 'col-sm-6', 'style': 'margin-bottom: 10px;'}).append(
-                    $('<div/>', {'class': 'card'}).append(
-                        $('<div/>', {'class': 'card-body'}).append(
-                            $('<h5/>', {'id': 'card_title', 'class': 'card-title', 'text': data[i].item_name})
-                        )
-                        .append($('<p/>', {'id': 'card_location', 'class': 'card-text', 'text': data[i].item_location})
-                        )
-                        .append($('<p/>', {'id': 'card_amount', 'class': 'card-text', 'text': data[i].item_amount})
-                        )
-                        .append($('<a/>', {'href': '#', 'class': 'btn btn-primary', 'text': 'Adjust Amount'})
-                        )
-    //                   Move this inline css out to a stylesheet later
-                        .append($('<a/>', {'href': '#', 'id': 'delete_item', 'class': 'btn btn-primary', 'text': 'Delete Item', 'style': 'margin-left: 5px;'})
-                        )
+            $('tbody').append(
+                $('<tr/>').append(
+                    $('<td/>', {'text': i+1})
+                ).append($('<td/>', {'text': data[i].item_name}))
+                .append($('<td/>', {'text': data[i].item_location}))
+                .append($('<td/>', {'text': data[i].item_amount}))
+                .append(
+                    $('<td/>').append(
+                        $('<button/>', {'class': 'btn btn-primary', 'text': 'Adjust Amount'})
+                        ).append($('<button/>', {'class': 'btn btn-danger', 'text': 'Delete Item', 'style': 'margin-left: 5px;'})
                     )
                 )
-            );
+            )
         }
-        let elem_text = $("#card_title").text()
-        console.log(elem_text)
+    });
+
+    $('button').click(function (){
+        let parent_id = $(this).parent().parent().parent().attr('id');
+        console.log(parent_id);
+        $('#'+ parent_id).remove();
     });
 });
