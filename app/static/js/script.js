316af06cc9d0b523e0e799c4269884f55df99051
Sundeep <mistrysundeep@gmail.com>
2022-12-16 00:10:40 +0000

    Button clicks now return their ids

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,24 +2,33 @@ $(document).ready(function (){
     $.get('/show_all_items', function(data, status){
         for(let i = 0; i < data.length; i++){
             $('tbody').append(
-                $('<tr/>').append(
+                $('<tr/>', {'id': 'item'+i}).append(
                     $('<td/>', {'text': i+1})
                 ).append($('<td/>', {'text': data[i].item_name}))
                 .append($('<td/>', {'text': data[i].item_location}))
                 .append($('<td/>', {'text': data[i].item_amount}))
                 .append(
-                    $('<td/>').append(
-                        $('<button/>', {'class': 'btn btn-primary', 'text': 'Adjust Amount'})
-                        ).append($('<button/>', {'class': 'btn btn-danger', 'text': 'Delete Item', 'style': 'margin-left: 5px;'})
+                    $('<td/>', {'style': 'text-align: center;'}).append(
+                        $('<button/>', {'id': 'adj'+i ,'class': 'btn btn-primary', 'text': 'Adjust Amount'})
+                        ).append($('<button/>', {'id': 'del'+i ,'class': 'btn btn-danger', 'text': 'Delete Item', 'style': 'margin-left: 5px;'})
                     )
                 )
             )
         }
     });
-
-    $('button').click(function (){
-        let parent_id = $(this).parent().parent().parent().attr('id');
-        console.log(parent_id);
-        $('#'+ parent_id).remove();
-    });
 });
+
+$(document).on('click', 'button', function (){
+//    list of rows in the table
+    let row_list = $('tbody').children();
+//    ids of the button clicked
+    let del_id = $(this).parent().parent().attr('id');
+    console.log(del_id);
+    console.log(row_list)
+//    prints last character of the string
+    console.log(del_id[del_id.length - 1])
+});
+
+
+
+
